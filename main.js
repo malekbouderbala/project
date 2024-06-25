9737d0549476d634f35291559f2fb12270632850
Your Name <bouderbalamalek9@gmail.com>
2024-06-25 11:46:43 +0100

    project

---
+++ main.js
@@ -0,0 +1,36 @@
+var CURRENTPOOL = 'middle-east';
+        var CURRENTLANGUAGE = 'fr';
+        var CURRENTCONDITION = 'tunisia';
+        var LOAD_PSYMA = '';
+          var ipadViewport = '<meta name="viewport" content="width=1024" />';
+        
+        var GLOBAL_CONFIG = GLOBAL_CONFIG || {};
+        
+        GLOBAL_CONFIG.currentMainNavigationArea = 'm-01-rd2013-homepage';
+        GLOBAL_CONFIG.currentMainNavigationArea_m101 = 'rd2013-homepage';
+        GLOBAL_CONFIG.pool = 'middle-east';
+        GLOBAL_CONFIG.countryCode = 'TN';
+        GLOBAL_CONFIG.language = 'fr';
+        GLOBAL_CONFIG.preferredLanguage = 'fr-FR';
+        GLOBAL_CONFIG.region = 'tunisia';
+        GLOBAL_CONFIG.loadPsyma = '';
+        GLOBAL_CONFIG.loadPsymaHost = '';
+        GLOBAL_CONFIG.home = true;
+        GLOBAL_CONFIG.dealersearch = {
+        porschedealerLocatorURL : ""
+        };
+        GLOBAL_CONFIG.tracking = {
+        id: ''
+        };
+
+        GLOBAL_CONFIG.isModelPages = false;
+        
+            GLOBAL_CONFIG.LIVE = true;
+          
+        GLOBAL_CONFIG.pageName = 'home';
+
+        
+        var GDL = null;
+
+        
+        var logonstate = 'false';
